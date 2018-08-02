1b38f302ba780a455f6a4524b2f09ffa1c5d3671
ValentynaKurovets <41925808+ValentynaKurovets@users.noreply.github.com>
2018-08-03 01:09:33 +0300

    Add files via upload

---
+++ QA_camp3_1/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[4];
+        numbers[0] = 1;
+        numbers[1] = 12;
+        numbers[2] = 6;
+        numbers[3] = 10;
+        Arrays.sort(numbers);
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        // write your code here
+    }
+}
