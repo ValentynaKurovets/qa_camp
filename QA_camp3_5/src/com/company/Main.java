5d52e65aeafc32a7f7d57f0b272c4d558cf144e7
ValentynaKurovets <41925808+ValentynaKurovets@users.noreply.github.com>
2018-08-03 11:23:07 +0300

    Add files via upload

---
+++ QA_camp3_5/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import com.sun.glass.ui.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int initialCapacity;
+        List <String> List= new ArrayList(0);
+
+        if (List.isEmpty()) {
+
+            throw new NullPointerException("Current collection is empty");
+
+        }
+    }
+}
