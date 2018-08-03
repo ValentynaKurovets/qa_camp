efda0c9810932769869fb56f12e59312af65f59c
ValentynaKurovets <41925808+ValentynaKurovets@users.noreply.github.com>
2018-08-03 11:28:58 +0300

    Add files via upload

--- QA_camp2/src/com/company/Main.java
+++ QA_camp2/src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        if (a<10 || a>10) {
-            System.out.print(a);
+        int a = 20;
+        if (a < 10) {
+            System.out.print("less then ten");
+        } else {
+            System.out.print("more then ten");
         }
     }
 }
